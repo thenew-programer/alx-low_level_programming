3523dc053f25b80d7b807e884f043eb14af547b8
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-16 04:04:32 -0700

    Added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-/*
+/**
+ * main - entry point
+ *
  * this program prints a string to the screen
+ *
+ * return always 0 (Sucess)
  */
 
 int main(void)
-{	/* this program prints a string to the console using puts */
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
