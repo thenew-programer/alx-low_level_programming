ad037072fd3fc97d768cae50fc1bd9350871cccf
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-16 03:53:23 -0700

    Added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/*
+ * this program prints a string to the screen
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
