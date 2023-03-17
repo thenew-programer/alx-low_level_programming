2748f1875492166129ffe352c5196d82675bd3b8
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-17 12:34:27 -0700

    Added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0
+*/
+
+int main(void)
+{
+int i, j, x, size;
+for (i = 48; i <= 57; ++i)
+{
+for (j = 48; j <= 57; ++j)
+{
+if ((i == j) || (i > j) || ((i == 56) && (j == 57)))
+{
+continue;
+}
+putchar(i);
+putchar(j);
+putchar(',');
+putchar(' ');
+}
+}
+x = 56;
+size = 3;
+while (1)
+{
+for (i = 0; i < size; i++)
+{
+if (i == 2)
+{
+x = 8;
+}
+putchar(x + i);
+}
+break;
+}
+}
