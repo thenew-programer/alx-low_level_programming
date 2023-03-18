2ee2266c6c99dea54d43fb1e19635ebd3faa1b1a
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-17 17:12:09 -0700

    Added

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
 
 #define SP ' '
-#define ENDL '\n'
 #define COMMA ','
 /**
- * main - Entry point
- * Return: 0
- */
+* main - Entry point
+* Return: 0
+*/
 
 int main(void)
 {
-    int i, j, k, l, x, size;
-    for (i = 48; i < 57; ++i)
-    {
-        for (j = 48; j <= 57; ++j)
-        {
-            for (k = 48; k <= 57; ++k)
-            {
-                for (l = 48; l <= 57; ++l)
-                {
-                    if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
-                    {
-                        continue;
-                    }
-                    else if (((i == k) && (j == l)) || ((k == l) && (l == i)))
-                    {
-                        continue;
-                    }
-                    else if ((i > j) || (i > k) || (i > l) || (j > l) || (j > k) || (k > l))
-                    {
-                        continue;
-                    }
-                    putchar(i);
-                    putchar(j);
-                    putchar(SP);
-                    putchar(k);
-                    putchar(l);
-                    putchar(COMMA);
-                    putchar(SP);
-                }
-            }
-        }
-    }
+int i, j, k, l, x, size;
+for (i = 48; i < 57; ++i)
+{
+for (j = 48; j <= 57; ++j)
+{
+for (k = 48; k <= 57; ++k)
+{
+for (l = 48; l <= 57; ++l)
+{
+if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+{
+continue;
+}
+else if (((i == k) && (j == l)) || ((k == l) && (l == i)))
+{
+continue;
+}
+else if ((i > j) || (i > k) || (i > l) || (j > l) || (j > k) || (k > l))
+{
+continue;
+}
+putchar(i);
+putchar(j);
+putchar(SP);
+putchar(k);
+putchar(l);
+putchar(COMMA);
+putchar(SP);
+}
+}
+}
+}
 
 x = 55;
 size = 5;
@@ -57,7 +56,7 @@ putchar(x + i);
 }
 break;
 }
-    return (0);
+return (0);
 }
 
 
