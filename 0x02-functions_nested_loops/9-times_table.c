91d4518e179444296fa4e550006d1de784ec812c
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-21 10:19:55 -0700

    Added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
                 _putchar((operation / 10) + '0');
                 _putchar((operation % 10) + '0');
             }
-            _putchar(10);
         }
+        _putchar(10);
     }
 }
