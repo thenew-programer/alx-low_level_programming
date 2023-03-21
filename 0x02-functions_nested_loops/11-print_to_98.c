6ce1d415e1a8769e539d44ef1a134dcd12be80e5
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-21 10:59:22 -0700

    Added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,36 +2,36 @@
 
 #define MAX 98
 /**
- * print_to_98 - print from 11 to 98
- * @n: the parameter that takes the function and start counting form
- * Return: 0as always
- */
+* print_to_98 - print from 11 to 98
+* @n: the parameter that takes the function and start counting form
+* Return: 0as always
+*/
 void print_to_98(int n)
 {
-    if (n == MAX)
-    {
-        printf("98\n");
-    }
-    while(n != MAX)
-    {
-        if (n > MAX)
-        {
-            printf("%d, ", n);
-            if (n == 99)
-            {
-                printf("98\n");
-            }
-            n--;
-        }
-        else
-        {
-            printf("%d, ", n);
-            if (n == 97)
-            {
-                printf("98\n");
-            }
-            n++;
-        }
+if (n == MAX)
+{
+printf("98\n");
+}
+while(n != MAX)
+{
+if (n > MAX)
+{
+printf("%d, ", n);
+if (n == 99)
+{
+printf("98\n");
+}
+n--;
+}
+else
+{
+printf("%d, ", n);
+if (n == 97)
+{
+printf("98\n");
+}
+n++;
+}
 
-    }
+}
 }
