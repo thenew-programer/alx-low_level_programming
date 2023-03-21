c9fba40bfeec56b083d106eb938ebb7b337bddef
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-21 08:01:44 -0700

    Added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,13 +14,3 @@ print_alphabet();
 return (0);
 }
 
-
-void print_alphabet(void)
-{
-char i;
-for (i = 'a'; i <= 'z'; ++i)
-{
-_putchar(i);
-}
-_putchar(10);
-}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * description: this func print the alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; ++i)
+{
+_putchar(i);
+}
+_putchar(10);
+}
