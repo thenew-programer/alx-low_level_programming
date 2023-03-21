aeb4d37c98064aba5bdfff7eabed9c80323345a7
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-21 08:35:14 -0700

    Added

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,7 @@
 
 int _isalpha(int c)
 {
-if (_islower(c) == 1 || (c >= 65 && c <= 90))
+if ((c >= 97 && c <= 12) || (c >= 65 && c <= 90))
 {
 return (1);
 }
@@ -17,3 +17,7 @@ else
 return (0);
 }
 }
+
+
+
+if ()
