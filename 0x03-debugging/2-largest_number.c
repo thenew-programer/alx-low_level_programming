bd73c1e7dae89fbb91340e4821fc5a9581e2e3fa
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-22 12:20:27 -0700

    Added

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -3,8 +3,9 @@
 /**
 * largest_number - returns the largest of 3 numbers
 * @a: first integer
+* @b: second integer
 * @c: third integer
-* Return: largest number
+* Return: largest number 0
 */
 
 int largest_number(int a, int b, int c)
