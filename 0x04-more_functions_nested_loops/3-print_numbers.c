ea41dab5c2d7d8da936c125e833920131843e31b
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-23 16:32:59 -0700

    Added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,15 @@
 
 /**
 * print_numbers - prints a set of numbers from 0 to 9
-* Return: 0
+*
+* Return: The numbers from  0 - 9
 */
 
 void print_numbers(void)
 {
 int i;
 
+i = 0;
 while (i < 10)
 {
 _putchar(('0' + i));
