00423a9bcd65a06b65b9b3d90c515b796e33135e
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-23 15:37:06 -0700

    Added

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,9 +15,9 @@ void print_square(int size)
         exit(EXIT_FAILURE);
     }
 
-    for (i = 0; i < n; i++)
+    for (i = 0; i < size; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j < size; j++)
         {
             _putchar('#');
         }
