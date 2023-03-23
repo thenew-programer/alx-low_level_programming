47dd811dc428499414e35461d47404f7a431b315
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-23 04:35:30 -0700

    Added

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print form 0 - 9 except for 2 & 4
+ * Return: nothing
+ */
+
+int print_most_numbers(void)
+{
+    int i;
+    while (i < 10)
+    {
+        if (i == 2 || i == 4)
+        {
+            i++;
+            continue;
+        }
+        _putchar('0' + i);
+        i++;
+    }
+    _putchar(10);
+}
