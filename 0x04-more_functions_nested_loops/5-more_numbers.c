108af8b22be02c88a5d44c25ace1db338b23f7db
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-23 16:39:36 -0700

    Added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,14 @@ char endl;
 endl = 10;
 for (i = 0; i < 10; i++)
 {
-    for (j = 0; j < 15; j++)
-    {
-        if (j > 9)
-        {
-            _putchar((j / 10) + '0');
-        }
-        _putchar((j % 10) + '0');
-    }
+for (j = 0; j < 15; j++)
+{
+if (j > 9)
+{
+_putchar((j / 10) + '0');
+}
+_putchar((j % 10) + '0');
+}
 write(1, &endl, 1);
 }
 }
