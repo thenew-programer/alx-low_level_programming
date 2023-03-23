d8e15515eed593ed36852bdc06b41d31b1ae70c1
Youssef Bouryal <youssefboural02@gmail.com>
2023-03-23 21:47:50 +0000

    added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+#define ENDL 10
+/**
+ * more_numbers - Prints from 0 - 14, 10 times
+ * Return: the output
+ */
+
+void more_numbers(void)
+{
+int i;
+char endl;
+
+endl = 10;
+for (i = 0; i < 15; i++)
+{
+    if (i > 9)
+    {
+        _putchar((i / 10) + '0');
+        _putchar((i % 10) + '0');
+        continue;
+    }
+    _putchar('0' + i);
+}
+write(1, &endl, 1);
+}
