66b32cab03329878f8135101ad479273e489a9a5
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-23 04:14:50 -0700

    Added

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * _isupper - checks if the char given is uppercase or
- * lower case
- * @c: the function parameter of type char
- * Return: 0 or 1
- */
+* _isupper - checks if the char given is uppercase or
+* lower case
+* @c: the function parameter of type char
+* Return: 0 or 1
+*/
 
 int _isupper(int c)
 {
-    int i;
+int i;
 
-    if (c > 64 && c < 91)
-    {
-        i = 1;
-    }
-    else
-    {
-        i = 0;
-    }
-    return (i);
+if (c > 64 && c < 91)
+{
+i = 1;
+}
+else
+{
+i = 0;
+}
+return (i);
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: the resutl 
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
