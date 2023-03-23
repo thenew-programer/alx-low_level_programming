ff50c17d9ad15ec8dc4a6cbdcb53bca008f60b83
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-23 16:38:59 -0700

    Added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,20 @@
 
 void more_numbers(void)
 {
-int i;
+int i, j;
 char endl;
 
 endl = 10;
-for (i = 0; i < 15; i++)
+for (i = 0; i < 10; i++)
 {
-if (i > 9)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-continue;
-}
-_putchar('0' + i);
-}
+    for (j = 0; j < 15; j++)
+    {
+        if (j > 9)
+        {
+            _putchar((j / 10) + '0');
+        }
+        _putchar((j % 10) + '0');
+    }
 write(1, &endl, 1);
+}
 }
