d54686c789a130ee00f315b06537b41e8d232c73
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-23 04:08:27 -0700

    Added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the char given is uppercase or
+ * lower case
+ * @c: the function parameter of type char
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+    int i;
+
+    if (c =< 90 && c >= 65)
+    {
+        i = 1;
+    }
+    else
+    {
+        i = 0;
+    }
+    return (i);
+}
