e6bb2cc325a419b9a6a77c44fa427b03494c594e
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-24 07:12:57 -0700

    Added

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: this program finds the largest prime factor of the
+ * number: 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+    long long i;
+    long long factor;
+    const long long number = 612852475143;
+
+    for (i = 0; i < number; i*i)
+    {
+
+    }
+    return (0);
+}
