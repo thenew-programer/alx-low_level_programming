3b52a49dc438ea2da70590c4b67d2ce822d0f0db
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-27 07:09:19 +0000

    added

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * int_strlen - this func return the the length of a string
- * @s: the pointer to the first char in the string
- * Return: the len or strlen
- */
+* _strlen - this func return the the length of a string
+* @s: the pointer to the first char in the string
+* Return: the len or strlen
+*/
 
 int _strlen(char *s)
 {
-    int strlen, i;
+int strlen, i;
 
-    strlen = i = 0;
+strlen = i = 0;
 
-    while (s[i] != 0)
-    {
-        strlen++;
-        i++;
-    }
-    return (strlen);
+while (s[i] != 0)
+{
+strlen++;
+i++;
+}
+return (strlen);
 }
