501b5a41610dc4080cb5136e288d4b39a343bf57
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-27 06:52:35 +0000

    edited

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - this function reset what inside the address to 98
- * @*n: the address of the actual parameter
- * Return: 0
- */
+* reset_to_98 - this function reset what inside the address to 98
+* @n: the address of the actual parameter
+* Return: 0
+*/
 
 void reset_to_98(int *n)
 {
-    *n = 98;
+*n = 98;
 }
