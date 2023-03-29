682f09324a47456eb67f09c0415331bf3b608882
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-29 23:26:42 +0000

    added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - this func concatenates two strings
+ *
+ * @dest: the pointer to the destination of the strings
+ * @src:  the pointert where is the string
+ *
+ * Return: the pointer of the concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int i, counter, counter_S, counter_D;
+    i = counter_S = counter_D = 0;
+    /* Count how many chars in the src string*/
+    while (src[counter_S] != 0)
+    {
+        counter_S++;
+    }
+    /* Count how many chars in the dest string*/
+    while (dest[counter_D] != 0)
+    {
+        counter_D++;
+    }
+
+    /* Concatenate the src with the dest string in the dest*/
+    for (i = counter_D; i < counter_S; i++)
+    {
+        dest[i] = src[i];
+    }
+    dest[i + 1] = '\0';
+    return (dest);
+}
