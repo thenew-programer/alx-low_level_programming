38034cc654d81870982b9b07a93fa4dbde252269
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-30 00:36:17 +0000

    added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ *
+ * Return: pointer to the copies string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int counter_D, counter_S;
+    counter_D = counter_S = 0;
+    while (dest[counter_D] != 0)
+    {
+        counter_D++;
+    }
+
+    while (counter_S < n)
+    {
+        if (src[counter_S] == 0)
+        {
+            counter = counter_S;
+            while (counter_D >= counter)
+            {
+                src[counter++] = '\0';
+            }
+            break;
+        }
+        dest[counter_S] = src[counter_S];
+        counter_S++;
+    }
+    return (dest);
+}
