543f0e0e83fe9b3df6ff8f96347db634f76ec035
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-30 01:36:23 +0000

    added

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * string_toupper - from lower case to upper case
- *
- * @c: the parametr
- *
- * Return: pointer to the string
- */
+* string_toupper - from lower case to upper case
+*
+* @c: the parametr
+*
+* Return: pointer to the string
+*/
 
 char *string_toupper(char *c)
 {
-    int i;
+int i;
 
-    for (i = 0; s[i] != 0; i++)
-    {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            s[i] = s[i] - 32;
-        }
-    }
-    
-    return (c);
+for (i = 0; s[i] != 0; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+}
+
+return (c);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
+char *string_toupper(char *);
+
 #endif /*MAIN_H*/
