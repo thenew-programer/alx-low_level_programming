38cdf2a1c57fa14173a16951c12cebb2b9ec95ac
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-30 00:09:58 +0000

    added

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings but defferent than strcat
- * because it tells you how much characters to add from the src
- *
- * @dest: pointer to the destination string
- * @src:  pointer to the source string
- * @n: the buffer of how much characters you want to add from the src
- *
- * Return: pointer to the dest string
- */
+* _strncat - concatenates two strings but defferent than strcat
+* because it tells you how much characters to add from the src
+*
+* @dest: pointer to the destination string
+* @src:  pointer to the source string
+* @n: the buffer of how much characters you want to add from the src
+*
+* Return: pointer to the dest string
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
@@ -24,10 +24,9 @@ counter_D++;
 counter = counter_D + n;
 for (i = counter_D; i < counter; i++)
 {
-    dest[i] = src[counter_S];
-    counter_S++;
+dest[i] = src[counter_S++];
 }
-    dest[counter + 1] = 0;
-    return (dest);
+dest[counter + 1] = 0;
+return (dest);
 }
 
