a862c5001f38d447fcafd860973b7391bc994d49
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-30 01:58:53 +0000

    added

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- *
- * @s: pointer to string
- *
- * Return: pointer to strng
- */
+* leet - encodes a string into 1337
+*
+* @s: pointer to string
+*
+* Return: pointer to strng
+*/
 
 char *leet(char *s)
 {
-    int i, j;
+int i, j;
 
-    char *a = "aAeEoOtTlL";
-    char *b = "4433007711";
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            if (s[i] == a[j])
-            {
-                s[i] = b[j];
-            }
-        }
-    }
-    return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+}
+}
+}
+return (s);
 }
