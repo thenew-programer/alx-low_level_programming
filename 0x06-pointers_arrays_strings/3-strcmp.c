c93e005a03a1874034c17f9b0479cf4ff6813783
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-30 01:08:52 +0000

    added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int cmp;
+    int cmp, i;
     
     cmp = 0;
+    i = 0;
 
     while (s1[i] != 0 || s2[i] != 0)
     {
         cmp += s1[i] - s2[i];
+        i++;
     }
 
     return (cmp);
