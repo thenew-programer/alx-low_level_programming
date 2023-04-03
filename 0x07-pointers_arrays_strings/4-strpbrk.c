df597a6067309b985aa875e0838dc1a9ef71d5d4
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-03 22:40:37 +0000

    added

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurence in the string s of any
+ * bytes in the string accept
+ * @s: the pointet to the string s
+ * @accept: the pointer to the string accept
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+    int i, length, A_len, A_Index;
+    int *index;
+
+    A_len = strlen(accept);
+    index = (int *)malloc(sizeof(int) * A_len);
+    A_Index = 0;
+    while (A_Index < A_len)
+    {
+        i = 0;
+        while (s[i] != 0)
+        {
+            if (s[i] == accept[A_Index])
+            {
+                index[A_Index] = i;
+                break;
+            }
+            i++;
+        }
+        A_Index++;
+    }
+
+    length = INT_MAX;
+    for (i = 0; i < A_len; i++)
+    {
+        if (index[i] < length)
+        {
+            length = index[i];
+        }
+    }
+    free(index);
+    return (s + length);
+}
