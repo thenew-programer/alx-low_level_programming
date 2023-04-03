c9527af86dc9684f0799a251c3651931b4043b00
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-03 22:18:19 +0000

    added

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to the string
+ * @accept: the bytes
+ * Return: number of bytes that consist of bytes accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    int i, length, A_len, A_Index;
+    int *index;
+    A_len = strlen(accept);
+    index = (int *)malloc(sizeof(int) * strlen(accept));
+
+    A_Index = 0;
+    while (A_Index < A_len)
+    {
+        i = 0;
+        while (s[i] != 0)
+        {
+            if (s[i] == accept[A_Index])
+            {
+                index[A_Index] = i + 1;
+                break;
+            }
+            i++;
+        }
+        A_Index++;
+    }
+
+    length = INT_MIN;
+    for (i = 0; i < A_len; i++)
+    {
+        if (index[i] > length)
+        {
+            length = index[i];
+        }
+    }
+    free(index);
+    return (length);
+}
