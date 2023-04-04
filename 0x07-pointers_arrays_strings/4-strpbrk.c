696089f1ab273f3af6faa5d33c1591b765740f8d
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-04 01:05:39 +0000

    added

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,11 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
+int j;
 
-for (i = 0; *s != 0; i++)
+while(*s)
 {
-for (j = 0; accept[j] != 0; j++)
+for (j = 0; accept[j]; j++)
 {
 if (*s == accept[j])
 {
