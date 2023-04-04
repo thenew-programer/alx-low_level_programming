082f9df31428c0524797b7ffb0ab9b2f79558307
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-04 01:06:46 +0000

    added

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 
 /**
 * _strpbrk - searchers astring for any of a set of bytes
-* @s: pointer to the string 
+* @s: pointer to the string
 * @accept: stringcontaining the bytes
 *
 * Return: pointer to the byte in s that matches one of the bytes
@@ -11,7 +11,7 @@ char *_strpbrk(char *s, char *accept)
 {
 int j;
 
-while(*s)
+while (*s)
 {
 for (j = 0; accept[j]; j++)
 {
