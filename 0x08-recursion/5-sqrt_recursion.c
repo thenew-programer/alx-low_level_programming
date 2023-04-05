2447472812e1453471801455aea54363ffc812be
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-05 15:45:44 +0000

    edited

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,8 +2,8 @@
 
 /**
 * _sqrt_recursion - return the natural square root of a number.
-* _sqrt_recursion_ex - function that helped me calc the sqrt of n.
 * @n: the number.
+* _sqrt_recursion_ex - function that helped me calc the sqrt of n.
 * @x: parameter that heped me find the sqrt root of n.
 * Return: the square root of n.
 */
@@ -16,7 +16,7 @@ if (n < 1)
 {
 return (-1);
 }
-return _sqrt_recursion_ex(n,1);
+return (_sqrt_recursion_ex(n, 1));
 }
 
 int _sqrt_recursion_ex(int n, int x)
