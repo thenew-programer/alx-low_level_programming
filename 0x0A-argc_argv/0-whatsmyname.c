d4233d1a8e21fe6bea0bd390668242ef07fb4a40
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-07 21:56:27 +0000

    added the readme file and the first file of the project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Program that prints its name followed by a new line
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# ARGC ARGV
+This repo is about command line argument in c, or in other words, the parameter that the main function get in C language.
