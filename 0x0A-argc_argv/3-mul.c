24a54de7e76f2eae75548c1c839c02d396f7e5e8
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-07 23:21:21 +0000

    aded

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers and print the result
- * @argc: the argument counter
- * @argv: the array of the argument
- * Return: 0
- */
+* main - multiplies two numbers and print the result
+* @argc: the argument counter
+* @argv: the array of the argument
+* Return: 0
+*/
 
 int main(int argc, char **argv)
 {
 
-    int x, y, result;
+int x, y, result;
 
-    if (argc < 3)
-    {
-        puts("Error");
-        return (1);
-    }
+if (argc < 3)
+{
+puts("Error");
+return (1);
+}
 
-    x = atoi(argv[1]);
-    y = atoi(argv[2]);
-    result = x * y;
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+result = x * y;
 
-    printf("%d\n", result);
-    
-    return (0);
+printf("%d\n", result);
+
+return (0);
 }
 
