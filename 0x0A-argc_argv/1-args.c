374706e4568debb6b2cb1686f14ef23f70e127c1
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-07 23:16:49 +0000

    modify the 1 file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - this program prints the number of
- * arguments passed into it
- * Return: 0 in Success
- */
+* main - this program prints the number of
+* arguments passed into it
+* Return: 0 in Success
+*/
 
-int main(int argc, char **argv)
+int main(int argc, char __attribute__((__unused__)) **argv)
 {
-    printf("%d\n", (argc - 1));
-    return (0);
+printf("%d\n", (argc - 1));
+return (0);
 }
 
