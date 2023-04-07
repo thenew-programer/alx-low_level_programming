9dc7d6ce8ddf27d1feb64d998b0288369f6639d3
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-07 22:37:59 +0000

    added

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: this program prints all the arguments it receives
- * @argc: the argument counter
- * @argv: the array containing the arguments
- * Return: 0
- */
+* main - Entry point
+* Description: this program prints all the arguments it receives
+* @argc: the argument counter
+* @argv: the array containing the arguments
+* Return: 0
+*/
 
 int main(int argc, char **argv)
 {
-    int i;
+int i;
 
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
-    return (0);
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
