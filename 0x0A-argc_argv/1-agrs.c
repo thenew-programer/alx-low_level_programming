8d76fd738beeb5dbe1ef6714b9d514d4d621b6f1
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-07 22:30:11 +0000

    added

---
+++ 0x0A-argc_argv/1-agrs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: this program prints the number of
+ * arguments passed into it
+ * Return: 0 in Success
+ */
+
+int main(int argc, char **argv)
+{
+    printf("%d\n", (argc - 1));
+    return (0);
+}
