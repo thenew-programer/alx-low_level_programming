1fbc9221d9a295d5065af74840f3f54bb5ea9cff
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-07 22:37:25 +0000

    added

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: this program prints all the arguments it receives
+ * @argc: the argument counter
+ * @argv: the array containing the arguments
+ * Return: 0
+ */
+
 int main(int argc, char **argv)
 {
     int i;
