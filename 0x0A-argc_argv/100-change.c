44838382ba6d4dd4d05c985fbb7a69b4f466783a
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-08 00:14:55 +0000

    addded

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,45 +2,45 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change
- * for an amount of money
- * @argc: argument counter
- * @argv: array of argument
- * Return: 0 on succes and 1 otherwise
- */
+* main - prints the minimum number of coins to make change
+* for an amount of money
+* @argc: argument counter
+* @argv: array of argument
+* Return: 0 on succes and 1 otherwise
+*/
 
 int main(int argc, char **argv)
 {
-    int cents, coins, i;
-    const int change[] = { 25, 10, 5, 2, 1 };
+int cents, coins, i;
+const int change[] = { 25, 10, 5, 2, 1 };
 
-    if (argc != 2)
-    {
-        puts("Error");
-        return (1);
-    }
+if (argc != 2)
+{
+puts("Error");
+return (1);
+}
 
-    coins = i = 0;
-    cents = atoi(argv[1]);
-    if (cents < 0)
-    {
-        puts("0");
-        return (0);
-    }
-    while (cents != 0)
-    {
-        while (cents > 0)
-        {
-            cents -= change[i];
-            if (cents < 0)
-            {
-                cents += change[i];
-                break;
-            }
-            coins++;
-        }
-        i++;
-    }
-    printf("%d\n", coins);
-    return (0);
+coins = i = 0;
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+puts("0");
+return (0);
+}
+while (cents != 0)
+{
+while (cents > 0)
+{
+cents -= change[i];
+if (cents < 0)
+{
+cents += change[i];
+break;
+}
+coins++;
+}
+i++;
+}
+printf("%d\n", coins);
+return (0);
 }
