ab6d311091ef99fccef60787068998d30f803f17
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-11 23:04:18 +0000

    betty

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,21 +3,22 @@
 #include <stdlib.h>
 
 /**
- * _strdup - duplicate a string and put it in the heap.
- * @str: pointer to the string
- * Return: the pointer to the new string
- */
+* _strdup - duplicate a string and put it in the heap.
+* @str: pointer to the string
+* Return: the pointer to the new string
+*/
 
-char *_strdup(char *str) {
-    char *strdup;
-    int length;
+char *_strdup(char *str)
+{
+char *strdup;
+int length;
 
-    if (str == NULL)
-        return (NULL);
-    length = strlen(str);
-    strdup = (char *) malloc(sizeof(char) * length + 1);
-    if (strdup == NULL) 
-        return (NULL);
-    strcpy(strdup, str);
-    return (strdup);
+if (str == NULL)
+return (NULL);
+length = strlen(str);
+strdup = (char *) malloc(sizeof(char) * length + 1);
+if (strdup == NULL)
+return (NULL);
+strcpy(strdup, str);
+return (strdup);
 }
