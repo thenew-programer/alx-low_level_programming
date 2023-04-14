5004256ee0fd5fe2c88163c32b8df677b71a64b8
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-14 00:47:49 +0000

    added

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,7 +30,7 @@ return (NULL);
 if (ptr == NULL)
 {
 /* Allocate the memory */
-newPtr = malloc(old_size + new_size);
+newPtr = malloc(new_size);
 if (newPtr == NULL)
 return (ptr);
 
@@ -52,5 +52,5 @@ memcpy(newPtr, ptr, old_size);
 free(ptr);
 return (newPtr);
 }
-return (NULL);
+return (ptr);
 }
