ecb81b9afc17013c051fed93f1c7aaafaf5ea790
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-17 07:33:47 +0000

    ADD: project 5 - free the dog you generate first free the string (name & owner) and then free the struct

---
+++ 0x0E-structure_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* free_dog - free the dog you generate
+* @d: the address where the dog lives
+* Return: Nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
