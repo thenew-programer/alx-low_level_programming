32719dbfa0e9d8299f956ae1a8abf45d19adef4d
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-17 13:13:52 -0700

    added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
-	* dog - custom data type that contains some dogs info
+	* struct dog - custom data type that contains some dogs info
 	* @name: the name of the dog
 	* @age: the age of the dog
 	* @owner: the name of the owner of the dog
