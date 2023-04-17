a0c3c4142ab60775ba7ecf09997a3597a1221b21
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-17 07:18:34 +0000

    add: project 2 - prints the content of a struct

--- 0x0E-structure_typedef/1-init_dog.c
+++ 0x0E-structure_typedef/1-init_dog.c
@@ -6,6 +6,7 @@
 * @name: the name of the dog
 * @age: the age of the dog
 * @owner: the name of the owner of the dog
+* Return: Nothing
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
---
+++ 0x0E-structure_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* print_dog - print what's inside the dog struct.
+* @d: the address of the struct
+* Return: nothing
+*/
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
