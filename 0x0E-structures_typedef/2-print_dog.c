ace88dd12a3d7aec2931feb68cce00ee81485de2
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-18 00:24:11 +0000

    added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,23 +9,10 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-
-
-	if (d->name != NULL)
+	if (d != NULL)
+	{
 		printf("Name: %s\n", d->name);
-	else
-		return;
-
-	if (d->age < 0)
 		printf("Age: %f\n", d->age);
-	else
-		return;
-
-	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
-	else
-		return;
-
+	}
 }
