38a107a25228cfd9d2cbae3c040c2aef0bdaa680
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-20 02:14:10 +0000

    Add: project 2 - function that searches for the index of an integer in an array using some function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int index, i;
 
 	index = -1;
-	if (size <= 0)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
 
 	for (i = 0; i < size; i++)
