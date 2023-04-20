5a8c73037581b8dd7083e2ac390fbb6222aed00c
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-20 00:30:15 +0000

    Add: project 0 - function that takes a name and a function pointer and use that function to print the name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+
+
+/**
+* print_name - prints a name
+* @name: the name pointer
+* @f: function pointer that will prints the name
+* Return: Nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_POINTER
+#define FUNC_POINTER
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
