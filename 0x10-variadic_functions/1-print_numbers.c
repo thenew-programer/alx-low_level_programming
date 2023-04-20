574c4b0bbd9fefc24078c8202215f63c2446c992
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-20 16:48:09 +0000

    Add: project 1 - function also takes many argument of type int and prints the all seperated by a delemiter given by the user

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
+	if (!separator)
+		return;
+
 	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
