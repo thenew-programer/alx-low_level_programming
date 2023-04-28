48ec2429f35d30eef6efc95bcbf399ab656f9ef1
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-29 00:40:26 +0100

    Add: project 1 - return the length of the same stupid linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* listint_len - returns the number of elements in a linked list.
+* @h: pointer.
+* Return: the length.
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t listLength;
+	
+	listLength = 0;
+	while (h)
+	{
+		listLength++;
+		h = h->next;
+	}
+
+	return (listLength);
+}
