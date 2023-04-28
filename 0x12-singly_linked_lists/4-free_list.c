c59ce0802226226be2ad65f0602474d65f2bb29c
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-28 02:10:45 +0100

    Add: project 4 - function that collect the garbage of ll

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* free_list - frees a list_t list.
+* @head: pointer to the head of the list
+* Return: nothing.
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (!head)
+		return;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
