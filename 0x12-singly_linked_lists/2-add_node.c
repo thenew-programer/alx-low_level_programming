2a5e45b38c7a0001585fc4fbf73bc218e0f101c4
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-28 01:26:34 +0100

    Update header file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+* add_node - adds a new node at the beginning of a list_t list.
+* @head: pointer to the head of the linked list.
+* @str: the string
+* Return: pointer to the created node.
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	return (newNode);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
 #ifndef LISTS
 #define LISTS
 
-#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
