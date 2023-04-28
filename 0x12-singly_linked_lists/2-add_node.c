e5ab79dacd9ccd1368b431db545ce9ea451500a2
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-28 01:33:36 +0100

    Add: project 2 - function that adds a node at the beginning of the ll

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,5 +10,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
+
+	newNode = (list_t *)malloc(sizeof(list_t));
+	if (!newNode)
+		return (NULL);
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+	newNode->next = *head;
+	*head = newNode;
 	return (newNode);
 }
