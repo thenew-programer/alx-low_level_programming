c6ae5cef4d220490a8943d4b24b012587b8b8cf4
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-04-29 02:50:52 +0100

    Add: project 10 - delete a node from the stupid ll at the given position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+#define SUCCEED 1
+#define FAILS -1
+/**
+* delete_nodeint_at_index - delete a node at a given index.
+* @head: pointer to pointer that points...
+* @index: position
+* Return: 1 if succeed -1 otherwise.
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *current, *tmp;
+
+	current = *head;
+	if (!*head)
+		return (FAILS);
+
+	if (index == 0)
+	{
+		current = current->next;
+		free(*head);
+		*head = current;
+		return (SUCCEED);
+	}
+
+	for (i = 0; (i < index) && (current->next); i++)
+	{
+		tmp = current;
+		current = current->next;
+	}
+
+	if (current)
+	{
+		tmp->next = current->next;
+		free(current);
+		return (SUCCEED);
+	}
+	return (FAILS);
+}
