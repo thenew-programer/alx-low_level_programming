053feff268926d962ce0fc83ae835df25a8a5ee7
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-01 14:17:08 +0100

    Add: project 9 - insert a new node a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		newNode->next = NULL;
+		newNode->next = *head;
 		*head = newNode;	
 		return (newNode);
 	}
