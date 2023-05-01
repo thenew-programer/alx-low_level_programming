e0a7d4c2dce9cb65b08bdeaca5bf701d2980d549
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-01 14:40:14 +0100

    Add: project 9 - insert a new node a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,30 +9,35 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *temp;
+	listint_t *newNode, *tmp;
 	unsigned int i = 0;
 
-	if (*head == NULL && idx != 0)
+	if (!*head && idx != 0)
 		return (NULL);
+
 	if (idx != 0)
 	{
-	temp = *head;
-	for (; i < idx - 1 && temp != NULL; i++)
-		temp = temp->next;
-	if (temp == NULL)
-		return (NULL);
+		tmp = *head;
+		for (; i < idx - 1 && tmp; i++)
+			tmp = tmp->next;
+
+		if (!tmp)
+			return (NULL);
 	}
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode)
 		return (NULL);
-	new_node->n = n;
+
+	newNode->n = n;
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
 	}
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+
+	newNode->next = tmp->next;
+	tmp->next = newNode;
+	return (newNode);
 }
