5906b2c2b8bee1dd69aea1faaf057ae69e81b47f
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-01 13:27:13 +0100

    Add: project 10 - delete a node from the stupid ll at the given position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,17 +16,17 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	current = *head;
 	if (!*head)
-		return (-1);
+		return (FAILS);
 
 	if (index == 0)
 	{
 		current = current->next;
 		free(*head);
 		*head = current;
-		return (1);
+		return (SUCCEED);
 	}
 
-	for (i = 0; (i < index) && (current->next); i++)
+	for (i = 0; (i < index) && (current); i++)
 	{
 		tmp = current;
 		current = current->next;
@@ -36,7 +36,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		tmp->next = current->next;
 		free(current);
-		return (1);
+		return (SUCCEED);
 	}
-	return (-1);
+	return (FAILS);
 }
