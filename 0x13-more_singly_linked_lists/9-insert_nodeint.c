eb9fc5d80a0783f294be33c0220f56c3b59dced2
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-01 14:35:48 +0100

    Add: project 9 - insert a new node a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 
-	if ((!*head && idx != 0) || !head)
+	if (!*head && idx != 0)
 		return (NULL);
 
 	newNode = (listint_t *)malloc(sizeof(listint_t));
