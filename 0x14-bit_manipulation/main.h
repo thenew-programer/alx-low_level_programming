c02875744bc42aa35b0dd4f624617dad7e24ce1d
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-04 16:30:10 +0100

    Update Header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,9 +6,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int _putchar(char c);
+
 unsigned int binary_to_uint(const char *b);
 
 void print_binary(unsigned long int n);
 
-int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
