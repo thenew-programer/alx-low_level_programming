564c373c762d1ceedcf6f0172a4d9edb077dca98
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-04 19:09:05 +0100

    Add: project 5 - return the number of bits to change in order to go from n to m

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* flit_bits - return the number of bits you would need to flip
+* to get from one number to another.
+* @n: the number.
+* @m: the number.
+* Return: number of bits you need to flip to get from n to m.
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bits ,i, j;
+
+	i = bits = 0;
+	j = sizeof(long int) * 8;
+
+	for (i = 0; i < j; i++)
+	{
+		if ((m & 1) != (n & 1))
+			bits++;
+
+		n >>= 1;
+		m >>= 1;
+	}
+	return (bits);
+}
