c8a13c2d0956b0505c5c8146a49286d0b42f79d8
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-04 15:20:02 +0100

    Update Header file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* print_binary - prints the binary representation of a number.
+* @n: the number.
+* Return: nothing.
+*/
+
+void print_binary(unsigned long int n)
+{
+
+}
