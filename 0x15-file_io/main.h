7a79240e769c133c5b411f3db34010339fa1403f
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-08 16:55:36 +0100

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,11 +2,12 @@
 #define MAIN
 
 
-#include <cstddef>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
+
+int _putchar(char c);
 #endif
