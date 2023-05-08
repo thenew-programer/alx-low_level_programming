f5b93f094730997556e4bcf3ce074d725611dbd4
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-08 17:20:45 +0100

    Update Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,9 @@
 #include <unistd.h>
 
 
+int _putchar(char c);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
-int _putchar(char c);
+int create_file(const char *filename, char *tesxt_content);
 #endif
