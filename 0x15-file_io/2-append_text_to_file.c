44d04c37e4953f946b31da1ec244ccdc158af62d
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-08 18:39:02 +0100

    Add: project 2 - append to a fileg

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,5 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	/* Append to the file using the fd return from open()*/
 	write(fd, text_content, strlen(text_content));
 
+	/* Close the file*/
+	close(fd);
 	return (SUCCESS);
 }
