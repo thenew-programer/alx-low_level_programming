c16d9d42a305636c53525019553908cdf9030c91
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-08 19:24:42 +0100

    .gitignore

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+#define BUFFER 1024 /* Size to read or write*/
+#define ARG_ERROR 97 /* ARGUMENT ERROR*/
+#define R_ERROR 98 /* READ ERROR*/
+#define W_ERROR 99 /*WRITE ERROR */
+#define C_ERROR 100	/* Close ERROR*/
+
 /**
 * main - entry point.
-* Description: This program copies the content of a file
+* Description: This program
+* copies the content of a file
 * to another file.
 * @argc: argument counter.
 * @argv: argument array.
@@ -11,34 +18,42 @@
 
 int main(int argc, char **argv)
 {
-	/* file descriptor for File_from and File_to respectively */
 	int fd1, fd2;
+	ssize_t n;
+	char string[BUFFER];
 
-	/* Check for number of arguments provided */
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
-		return (97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (ARG_ERROR);
 	}
-
-	/* Open file_from on read mode only*/
 	fd1 = open(argv[1], O_RDONLY);
-	/* if file_from doesn't exist*/
 	if (fd1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		return (R_ERROR);
 	}
-
-
-	/* Open file_to read only*/
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		return (99);
+		return (W_ERROR);
 	}
-
-
+	while ((n = read(fd1, string, BUFFER)) > 0)
+	{
+		write(fd2, string, BUFFER);
+	}
+	fd1 = close(fd1);
+	fd2 = close(fd2);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		return (C_ERROR);
+	}
+	else if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		return (C_ERROR);
+	}
 	return (EXIT_SUCCESS);
 }
