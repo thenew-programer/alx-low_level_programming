283e17a957d672aa3450bcf214a7e88ab6a47440
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-08 18:06:22 +0100

    Add: project 1 - create a file and write bytes to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,13 +18,10 @@ int create_file(const char *filename, char *text_content)
 		return (FAILURE);
 
 
-	printf("hello world\n");
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (FAILURE);
 
-
-	printf("hello bitch\n");
 	write(fd, text_content, strlen(text_content));
 	return (SUCCESS);
 }
