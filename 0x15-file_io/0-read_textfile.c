ad1bced6a19f555141190a92868e25db3e37e199
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-07-10 18:31:53 +0100

    first program

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* read_textfile - reads file and prints it to STDOUT
+* @filename: pointer to the file.
+* @letters: number of bytes to print.
+* Return: the number of bytes printed.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t byteRead, byteWrite;
+	int fd;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = (char *)malloc(sizeof(char) * (letters + 1));
+	if (!buffer)
+		return (0);
+
+	byteRead = read(fd, buffer, letters);
+	byteWrite = write(STDOUT_FILENO, buffer, byteRead);
+	if (byteWrite == -1)
+		return (0);
+
+	close(fd);
+	free(buffer);
+	return (byteWrite);
+}
