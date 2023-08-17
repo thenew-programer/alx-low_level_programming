7e47e9c4daccd8f7565cfe3b164fcfd84ac99875
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-17 20:52:24 +0100

    5th program

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,13 +9,26 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *node;
+	dlistint_t *tmp;
 	size_t len;
 	size_t i;
+	/*
 	len = dlistint_len(head);
+	*/
+
+
+	tmp = head;
+	len = 0;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		len++;
+	}
 	if (index > len)
 	{
 		return (NULL);
 	}
+
 	node = head;
 	for (i = 0; i < index; i++)
 	{
