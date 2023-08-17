c6a63906e7be88c112e7c5f217ee3b376a55e402
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-17 19:43:31 +0100

    1th program;

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - count the number of nodes in a doubly_linked_list
+ * @h: pointer to the head of the dll
+ * Return: number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len;
+	dlistint_t *tmp;
+
+	tmp = (dlistint_t *)h;
+	len = 0;
+	while (tmp)
+	{
+ 		tmp = tmp->next;
+		len++;
+	}
+	return (len);
+}
