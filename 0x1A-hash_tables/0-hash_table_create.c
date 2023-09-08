a93f3c9e9890db4dbb6e640b20d34069ff2cc5fd
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-08 16:32:52 +0100

    function that creates a hash table of a fixed size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "hash_tables.h"
 
 /**
  * hash_table_create - create a hash table
@@ -10,16 +10,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 
-	table->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
-	if (!table->array)
-	{
-		free(table);
+	table->size = size;
+	table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
 		return (NULL);
-	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
 	return (table);
 }
