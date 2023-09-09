301737f92a068dc10393c4612bc6c8e0ee4fbde7
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-09 14:45:59 +0100

    take care of edge cases

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *tmp;
 
-	if (!key)
+	if (!key !! !ht || !value)
 		return (FAILURE);
 	index = key_index((unsigned char *)key, ht->size);
 
