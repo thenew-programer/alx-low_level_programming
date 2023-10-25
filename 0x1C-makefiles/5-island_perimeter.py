81c3fedf5049bae1a2a8664d2e8cd5e411f411b6
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-10-25 21:58:33 +0100

    island_perimeter function

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""island perimeter"""
+
+
+def island_perimeter(grid):
+    """
+    Return the perimeter of the island described in grid
+
+    Args:
+        grid: is a list of lists of integers
+        """
+    perimeter = 0
+    edges = 0
+    for n in range(len(grid)):
+        for j in range(len(grid[n])):
+            if grid[n][j] == 1:
+                perimeter += 1
+                if (grid[n][j - 1] == 1):
+                    edges += 1
+                if (grid[n - 1][j] == 1):
+                    edges += 1
+    return perimeter * 4 - edges * 2
